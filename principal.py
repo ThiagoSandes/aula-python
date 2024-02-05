169616a3a41a7e29836ac8af0f49d62a483450e8
ThiagoSandes <thiago_ts2014@outlook.com>
2024-02-04 22:10:32 -0300

    adição de correções de bug

--- principal.py
+++ principal.py
@@ -2,7 +2,8 @@ import os
 # restaurantes = ['saborsertao', 'sabornenca']
 restaurantes = [{'nome': 'Saborsertao', 'categoria': 'Almoço', 'Ativação': True},
                 {'nome': 'NordesteJantar', 'categoria': 'Jantar', 'Ativação': False},
-                {'nome': 'Pizzariatodo', 'categoria': 'Pizza', 'Ativação': True}]
+                {'nome': 'Pizzariatodo', 'categoria': 'Pizza', 'Ativação': True}
+                ]
 
 
 def exibir_nome_do_programa():
@@ -40,32 +41,68 @@ def finalizar_app():
 
 
 def opcao_incorreta():
-    print('você inseriu uma opção incorreta\n!')
+    print('você inseriu uma opção incorreta!\n')
     limpar_terminal()
 
 
 def listar_restaurante():
-    exibir_nome('Listar Restaurantes Cadastrado:\n')
+    exibir_nome('Listar Restaurantes Cadastrados:\n')
     for nome_restaurante in restaurantes:
         filtro_nome = nome_restaurante['nome']
         filtro_categoria = nome_restaurante['categoria']
         filtro_status = nome_restaurante['Ativação']
-        filtro_status = 'Ativado' if filtro_status == True else 'Desativado'
+        filtro_status = 'Ativado' if filtro_status else 'Desativado'
         print(f'{filtro_nome}|{filtro_categoria}|{filtro_status}')
     limpar_terminal()
 
 
+def filtrar_restaurante():
+    while True:
+        filtro_restaurante = input('Informe Ativado ou Desativado: ')
+        if filtro_restaurante.lower() == 'ativado':
+            filtro_restaurante = True
+            break
+        elif filtro_restaurante.lower() == 'desativado':
+            filtro_restaurante = False
+            break
+        else:
+            print('Dados não correspondente')
+            continue
+    return filtro_restaurante
+
+
 def cadastrar_restaurante():
     exibir_nome('Cadastar Restaurante!\n')
-    nome_restaurante = input('Informe o nome do restaurante:')
-    restaurantes.append(nome_restaurante)
+    nome_restaurante = input('Informe o nome do restaurante: ')
+    categoria_restaurante = input('Informe o nome da categoria: ')
+    filtro_restaurante = filtrar_restaurante()
+    tratamento_restaurante = {'nome': nome_restaurante,
+                              'categoria': categoria_restaurante, 'Ativação': filtro_restaurante}
+    restaurantes.append(tratamento_restaurante)
     print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso!\n')
     limpar_terminal()
 
 
 def alterando_status_restaurante():
-    exibir_nome('Ativer ou Desativar Restaurente')
-
+    exibir_nome('Ativer ou Desativar Restaurente\n')
+    restaurante_status = input(
+        'Escreva o nome do restaurante que desejar ativar ou desativar: ')
+    restaurante_encontrado = False
+    for nome_restaurante in restaurantes:
+        if restaurante_status == nome_restaurante['nome']:
+            restaurante_encontrado = True
+            print(f'Restaurante Localizado {nome_restaurante['nome']} | {
+                  nome_restaurante['Ativação']}')
+            nome_restaurante['Ativação'] = not nome_restaurante['Ativação']
+            mensagem = f'O restaurante {restaurante_status} foi ativado com sucesso ' if nome_restaurante['Ativação'] else f'O restaurante {
+                restaurante_status} foi desativado.'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print('Restaurante não localizado.')
+        novo_cadastro = input(
+            'Deseja cadastrar um novo restaurante? (sim/nao): ')
+        if novo_cadastro.lower() == 'sim':
+            cadastrar_restaurante()
     limpar_terminal()
 
 
@@ -86,6 +123,7 @@ def escolher_opcao():
             opcao_incorreta()
     except:
         opcao_incorreta()
+    limpar_terminal()
 
 
 def main():
